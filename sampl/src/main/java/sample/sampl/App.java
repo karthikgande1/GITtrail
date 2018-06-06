9d7ea4d8796088187c1b7a790e13b84f5bc9d28b
Training_H3B.09.08 <Training_H3B.09.08@HDC3-D-447S0NX.dir.svc.accenture.com>
2018-06-06 16:19:02 +0530

    new

---
+++ sampl/src/main/java/sample/sampl/App.java
@@ -0,0 +1,13 @@
+package sample.sampl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
