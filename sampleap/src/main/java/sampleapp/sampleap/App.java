5513c3386d9e8b6f3ecba9d2de77f3c8274287cf
Training_H3B.09.08 <Training_H3B.09.08@HDC3-D-447S0NX.dir.svc.accenture.com>
2018-06-06 16:05:32 +0530

    initial commit to sample app maven project

---
+++ sampleap/src/main/java/sampleapp/sampleap/App.java
@@ -0,0 +1,13 @@
+package sampleapp.sampleap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
